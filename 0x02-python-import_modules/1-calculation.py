c4f3bf2e26851435f40d6e315e7cc2bc29e4182f
root <bustlezach001@gmail.com>
2023-01-05 17:40:40 -0800

    task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
-
-    addition = add(a, b)
-    difference = sub(a, b)
-    product = mul(a, b)
-    quotient = div(a ,b)
-
-    print('{:d} + {:d} = {:d}'.format(a, b, addition))
-    print('{:d} - {:d} = {:d}'.format(a, b, difference))
-    print('{:d} * {:d} = {:d}'.format(a, b, product))
-    print('{:d} / {:d} = {:d}'.format(a, b, quotient))
+    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
+    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
+    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
+    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
