b62c215293a6778c7dede1110a1c04cb259afa93
root <bustlezach001@gmail.com>
2023-01-24 19:38:54 -0800

    task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""Define a class"""
 
 class Square:
+    """Class name is square"""
+
     def __init__(self, size=0):
+        """Initialization of contractor args value to zero on default"""
+
         if not isinstance(size, int):
+            """check if args exists in the class attribute"""
+
             raise TypeError('size must be integer')
         if size < 0:
             raise ValueError('size must be >= 0')
