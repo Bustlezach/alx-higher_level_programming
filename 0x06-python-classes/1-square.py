e94dbe7419f1ad18a06b8120ec481034990f1d7b
root <bustlezach001@gmail.com>
2023-01-24 19:33:05 -0800

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,8 +2,12 @@
 """Defines a class"""
 
 class Square:
+    """square class"""
+
     def __init__(self, size):
-        """size contractor"""
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
 
         self.__size =size
         """private attribute of square class"""
