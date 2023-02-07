79138f9e05afdc446f40e4e78e1914c46837091e
root <bustlezach001@gmail.com>
2023-02-07 12:45:05 -0800

    task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    The list of attributes and methods of an object
+    is returned ny this module
+"""
+
+def lookup(obj):
+    """This functions looks out for all attributes and methods of an object"""
+    return dir(obj)
