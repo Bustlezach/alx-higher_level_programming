7ceb1a590b02e0f091d6259998acafbe9b43d3ed
root <bustlezach001@gmail.com>
2023-04-11 14:06:59 -0700

    sie

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 
 // This script prints the addition of 2 integers
 
-function add(a, b){
-  return a+b;
+function add (a, b) {
+  return a + b;
 }
 console.log(add(Number(process.argv[2]), Number(process.argv[3])));
