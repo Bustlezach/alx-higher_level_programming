68b8cb45b8db87febd5fd5838eb58106f9c528d8
root <bustlezach001@gmail.com>
2023-04-11 09:52:05 -0700

    task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,9 +2,9 @@
 
 // This script prints 3 lines using loop
 
-
 let i;
 const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 for (i = 0; i < 3; i++) {
   console.log(myString[i]);
 }
