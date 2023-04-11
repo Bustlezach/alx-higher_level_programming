c06a60ce3c47998649cafec22d31aea136b16d00
root <bustlezach001@gmail.com>
2023-04-11 07:28:29 -0700

    task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,5 @@
 
 // Print the values of argv except undefined.
 
-if (typeof process.argv[2] === 'undefined') {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-};
+
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
