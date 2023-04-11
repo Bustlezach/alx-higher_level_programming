8a021a2dd67bf17bc3d431f12090c2b16b29b6c2
root <bustlezach001@gmail.com>
2023-04-11 13:42:46 -0700

    sie

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
-// This script prints square 
+// This script prints square
 
 const size = Math.floor(Number(process.argv[2]));
-if(isNaN(size)){
-        console.log('Missing size');
+if (isNaN(size)) {
+  console.log("Missing size");
 } else {
-        for(let i = 0; i < size; i++){
-                console.log('x'.repeat(size));
-        }
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
 }
