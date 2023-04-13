931f567e813786e632a86607decfc4d8708a45f9
root <bustlezach001@gmail.com>
2023-04-13 08:42:20 -0700

    task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-
-const dict = require('./100-data.js').dict;
+const dict = require('./101-data.js').dict;
 
 const new_dict = {};
-
-for (const [key, value] of Object.entries(dict)) {
-  if (!new_dict[value]) {
-    new_dict[value] = [key];
-  } else {
-    new_dict[value].push(key);
+for (const key in dict) {
+  if (new_dict[dict[key]] === undefined) {
+    new_dict[dict[key]] = [];
   }
+  new_dict[dict[key]].push(key);
 }
-
 console.log(new_dict);
