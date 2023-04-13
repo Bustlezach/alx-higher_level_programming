2ac3c357d97f8e4c5a1d5f2410047f47496e1ff0
root <bustlezach001@gmail.com>
2023-04-13 06:40:02 -0700

    task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 
 const list = require('./100-data.js').list;
 
-const new_arr = list.map((cur, i) cur * i);
+const new_arr = list.map((cur, i) => cur * i);
 console.log(list);
 console.log(new_arr);
