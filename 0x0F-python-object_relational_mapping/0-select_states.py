93542575fc34b6a92a29aca4a5e8fcb73d739a36
root <bustlezach001@gmail.com>
2023-04-16 16:45:40 -0700

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This script accesses a MySQL database and retrieves states from it.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    """Establish a connection to the MySQL database"""
+    db_conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        port=3306,
+        db=sys.argv[3]
+    )
+
+    """Create a cursor object and execute a query to retrieve all states"""
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    state_rows = cursor.fetchall()
+
+    """Loop through each row and print it to the console"""
+    for state in state_rows:
+        print(state)
