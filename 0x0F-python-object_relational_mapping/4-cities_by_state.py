4fc4059a12d626f4c028c06c54e33cda2d66351d
root <bustlezach001@gmail.com>
2023-04-16 19:30:47 -0700

    task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,23 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """
-This script accesses a MySQL database and it truncates the result where necessary.
+This script connects to a MySQL database and retrieves all cities and their corresponding state names,
+and then prints them to the console.
 """
 
 import MySQLdb
 import sys
 
+# Check if the command line arguments are valid
+if len(sys.argv) != 4:
+    print(f"Usage: {sys.argv[0]} username password database_name")
+    sys.exit(1)
 
-if __name__ == '__main__':
-    argument = sys.argv
-    if (len(argument) != 4):
-        print("Usage: {} username password database_name".format(argument[0]))
-        exit(1)
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
-    """Establish a connection to the MySQL database"""
+# Get the command line arguments for database credentials
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+# Connect to the database
+try:
     db_conn = MySQLdb.connect(
         host="localhost",
         user=username,
@@ -25,15 +27,26 @@ if __name__ == '__main__':
         port=3306,
         db=database
     )
+except MySQLdb.Error as e:
+    print(f"Error connecting to the database: {e}")
+    sys.exit(1)
 
-    """Create a cursor object and execute a query to retrieve all states"""
+# Create a cursor object and execute a query to retrieve all cities and their corresponding state names
+try:
     cursor = db_conn.cursor()
-    number_row = cursor.execute("SELECT states.name, cities.id, cities.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;")
+    cursor.execute("SELECT states.name, cities.id, cities.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC")
     city_rows = cursor.fetchall()
-
-    """Loop through each row and print it to the console"""
-    for city in city_rows:
-        print(city)
-
+except MySQLdb.Error as e:
+    print(f"Error executing query: {e}")
     cursor.close()
     db_conn.close()
+    sys.exit(1)
+
+# Loop through each row and print it to the console
+for city in city_rows:
+    print(city)
+
+# Close the cursor and database connection
+cursor.close()
+db_conn.close()
+
