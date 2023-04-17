2feb4f491348e2d39a3104c3ec816b0af8b9541c
root <bustlezach001@gmail.com>
2023-04-17 07:30:29 -0700

    task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,8 +5,8 @@ from the database `hbtn_0e_14_usa`.
 """
 
 from sys import argv
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -19,15 +19,14 @@ if __name__ == "__main__":
     db_conn = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db_conn)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
+    cal_state = State(name='California')
+    san_francisco_city = City(name='San Francisco')
+    cal_state.cities.append(san_francisco_city)
 
-    Query = session.query(City, State).join(State)
-
-    for city, state in Query.all():
-        print("{}: ({:d}) {}".format(state.name, city.id, city.name))
-
+    session.add(cal_state)
     session.commit()
     session.close()
-
