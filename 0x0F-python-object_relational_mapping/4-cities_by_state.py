11cba847c09af390ea287c0636f7a7c20995a365
root <bustlezach001@gmail.com>
2023-04-16 19:33:37 -0700

    task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,52 +1,30 @@
 #!/usr/bin/python3
-
 """
-This script connects to a MySQL database and retrieves all cities and their corresponding state names,
-and then prints them to the console.
-"""
+Created on Sat Aug  8 09:05:11 2020
 
+@author: Robinson Montes
+"""
 import MySQLdb
 import sys
 
-# Check if the command line arguments are valid
-if len(sys.argv) != 4:
-    print(f"Usage: {sys.argv[0]} username password database_name")
-    sys.exit(1)
 
-# Get the command line arguments for database credentials
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-# Connect to the database
-try:
-    db_conn = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        password=password,
-        port=3306,
-        db=database
-    )
-except MySQLdb.Error as e:
-    print(f"Error connecting to the database: {e}")
-    sys.exit(1)
-
-# Create a cursor object and execute a query to retrieve all cities and their corresponding state names
-try:
-    cursor = db_conn.cursor()
-    cursor.execute("SELECT states.name, cities.id, cities.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC")
-    city_rows = cursor.fetchall()
-except MySQLdb.Error as e:
-    print(f"Error executing query: {e}")
-    cursor.close()
-    db_conn.close()
-    sys.exit(1)
-
-# Loop through each row and print it to the console
-for city in city_rows:
-    print(city)
-
-# Close the cursor and database connection
-cursor.close()
-db_conn.close()
-
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT cities.id, cities.name, states.name\
+                           FROM cities INNER JOIN states\
+                           ON cities.state_id=states.id\
+                           ORDER BY cities.id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
