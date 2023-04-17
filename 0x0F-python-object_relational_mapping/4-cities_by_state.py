5ff5f11190f5a3f76d599e89f77330990b513728
root <bustlezach001@gmail.com>
2023-04-16 19:20:55 -0700

    task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,6 +9,10 @@ import sys
 
 
 if __name__ == '__main__':
+    argument = sys.argv
+    if (len(argument) != 4):
+        print("Usage: {} username password database_name".format(argument[0]))
+        exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -24,7 +28,7 @@ if __name__ == '__main__':
 
     """Create a cursor object and execute a query to retrieve all states"""
     cursor = db_conn.cursor()
-    cursor.execute("SELECT states.name, cities.id, cities.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    cursor.execute("SELECT states.name, cities.id, cities.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC")
     city_rows = cursor.fetchall()
 
     """Loop through each row and print it to the console"""
