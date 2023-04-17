29c60627e23449d29a1203bd8f9a4d674d624aac
root <bustlezach001@gmail.com>
2023-04-17 05:21:16 -0700

    task

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script screens out all State objects containing 'a' from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    arguments = sys.argv
+    if len(arguments) != 4:
+        print(f"Usage: {arguments[0]} username password db_name")
+        exit(1)
+
+    username = arguments[1]
+    password = arguments[2]
+    data = arguments[3]
+
+    # Connect to the database and get a state from the database.
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{data}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.contains('a')):
+        print(f"{state.id}: {state.name}")
