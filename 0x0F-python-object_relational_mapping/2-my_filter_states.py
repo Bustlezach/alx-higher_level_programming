b8de2f838e26916fac8eaef8b3d0d9ec8da0eb86
root <bustlezach001@gmail.com>
2023-04-16 17:54:22 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     """Create a cursor object and execute a query to retrieve all states"""
     cursor = db_conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(sys.argv[4],))
     state_rows = cursor.fetchall()
 
     """Loop through each row and print it to the console"""
