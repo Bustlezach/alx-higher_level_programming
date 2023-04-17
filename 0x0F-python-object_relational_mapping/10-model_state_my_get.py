65736ff18808eb68006d53cc27082562516b775c
root <bustlezach001@gmail.com>
2023-04-17 05:45:49 -0700

    task

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 """
-This script screens out all State objects with the name passed as argument from the database hbtn_0e_6_usa
+This script prints the State object id
+with the name passed as argument
+from the database `hbtn_0e_6_usa`.
 """
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-if __name__ == '__main__':
-    arguments = sys.argv
-    if len(arguments) != 5:
-        print(f"Usage: {arguments[0]} username password db_name state_name")
-        exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    username = arguments[1]
-    password = arguments[2]
-    data = arguments[3]
-    state_name = arguments[4]
-
-    # Connect to the database and get a state from the database.
-    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{data}")
+    db_uri = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
-    session = Session()
 
+    session = Session()
     state = session.query(State).filter(State.name == state_name).first()
+
     if state is None:
-        print('Not found')
+        print("Not found")
     else:
         print(f"{state.id}")
