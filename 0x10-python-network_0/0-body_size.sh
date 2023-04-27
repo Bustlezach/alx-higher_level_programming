bfb877fa12f2ba2ac68c8da34aaebc59afc49cad
root <bustlezach001@gmail.com>
2023-04-27 15:06:23 -0700

    task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script displays the size of the body of the response
-curl -si  https://example.com | grep -i content-length | cut -f2 -d' '
+curl -si  '$1' | grep -i content-length | cut -f2 -d' '
