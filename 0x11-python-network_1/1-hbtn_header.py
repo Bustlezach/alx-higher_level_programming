8d862d6bb54219a1d2dca00f1067b53939fc00e6
root <bustlezach001@gmail.com>
2023-04-29 07:09:41 -0700

    task 0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,12 +2,14 @@
 """This script  displays the value of the X-Request-Id"""
 
 from urllib import request
+from sys import argv
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = argv[1]
     url_req = request.Request(url)
     with request.urlopen(url_req) as response:
         header = response.info()
         # print(header)
         x_req_id = header['X-Request-Id']
         print(x_req_id)
+
