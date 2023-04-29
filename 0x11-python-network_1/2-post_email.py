6849907c16bed3382279ae236ff186875a9ab613
root <bustlezach001@gmail.com>
2023-04-29 08:00:00 -0700

    task 0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+from urllib import request
+from urllib import parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {'mail': argv[2]}
+    mail = parse.urlencode(value)
+    data = mail.encode('ascii')
+    url_req = request.Request(url, data)
+    with request.urlopen(url_req) as resp:
+        body = resp.read()
+        body_str = body.decode('utf-8')
+        print(body_str)
