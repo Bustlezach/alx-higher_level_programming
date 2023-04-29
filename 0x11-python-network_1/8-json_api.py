9891afd67fad1091829b37b102547fcd0b72e9ab
root <bustlezach001@gmail.com>
2023-04-29 14:54:09 -0700

    task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,18 +7,18 @@ from requests import post
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        q = {'key': argv[1]}
-    else:
-        q = {'key': ''}
     url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = {'q': argv[1]}
+    else:
+        q = {'q': ''}
     r = post(url, params=q)
     try:
-        data = r.json()
-        if len(data) == 0:
+        obj = r.json()
+        if len(obj) == 0:
             print('No result')
         else:
-            print('[{}] {}'.format(data['id'], data['name']))
+            print('[{}] {}'.format(obj['id'], obj['name']))
     except ValueError:
         print('Not a valid JSON')
 
