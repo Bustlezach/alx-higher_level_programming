e4421317eb8d0a90f2ce344bae252eeffcae7e2d
root <bustlezach001@gmail.com>
2023-04-30 00:56:23 -0700

    Advance

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""
-This script takes in a letter and sends a POST request to
-'http://0.0.0.0:5000/search_user' with the letter as a parameter.
-"""
-from requests import post, get
+"""This script uses the GitHub API to display a GitHub ID based on given credentials."""
+
 from sys import argv
+from requests import post, get
+
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     if len(argv) > 1:
-        q = {'q': argv[1]}
+        q = {'key': argv[1]}
     else:
-        q = {'q': ''}
+        q = {'key': ''}
     r = post(url, params=q)
     try:
-        resp = r.json()
-        if resp == {}:
-            print("No result")
+        obj = r.json()
+        if obj == {}:
+            print('No result')
         else:
-            print("[{}] {}".format(resp.get("id"), resp.get("name")))
+            print('[{}] {}'.format(obj.get('id'), obj.get('name')))
     except ValueError:
-        print("Not a valid JSON")
-
+        print('Not a valid JSON')
