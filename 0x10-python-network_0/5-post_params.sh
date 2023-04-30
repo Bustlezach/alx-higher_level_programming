4e154c8d1d195d4bed3cb3a8199d87154f03aa86
root <bustlezach001@gmail.com>
2023-04-30 03:12:59 -0700

    Advance

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,4 +2,4 @@
 # This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 email='test@gmail.com'
 subject='I will always be here for PLD'
-curl -sX POST -d "email=$email&subject=$subject" "$1"
+curl -s -X POST -d "email=$email&subject=$subject" "$1"
