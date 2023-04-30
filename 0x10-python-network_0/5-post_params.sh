a44219ea310a85c17a53a23c4be91a796b8e8814
root <bustlezach001@gmail.com>
2023-04-30 03:39:34 -0700

    Advance

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# POSTs request to  URL,displays the body of the response
-email=test@gmail.com
-subject=I will always be here for PLD
-curl -s -d "email=$email&subject=$subject" "$1"email='test@gmail.com'
+# POSTs request to URL, displays the body of the response
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
