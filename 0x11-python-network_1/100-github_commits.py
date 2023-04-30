a0c868f00e989a76844caec809f203e73cb7c17e
root <bustlezach001@gmail.com>
2023-04-30 00:44:32 -0700

    Advance

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,17 +6,18 @@ from requests import get
 
 
 if __name__ == "__main__":
-    url = f'https://developer.github.com/{argv[2]}/{argv[1]}/commits/'
+    usr = argv[2]
+    repo = argv[1]
+    url = f'https://developer.github.com/{usr}/{repo}/commits/'
 
     r = get(url)
     obj = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(
-                obj[i].get("sha"),
-                obj[i].get("commit").get("author").get("name")))
-            if i == 9:
+            print('{}: {}'.format(obj[i].get('sha'), obj[i].get('commit').get('author').get('name')))
+            if i < 10:
+                print('Commit is less than 10')
                 break
-    except IndexError:
+    except ValueError:
         pass
-
+    
