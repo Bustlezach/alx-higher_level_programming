083cd372e23c332954f6233bce4978ab33069474
root <bustlezach001@gmail.com>
2023-04-30 07:13:47 -0700

    task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -os /dev/null -w "%{code}" "$1"
+curl -o -s /dev/null -w "%{code}" "$1"
