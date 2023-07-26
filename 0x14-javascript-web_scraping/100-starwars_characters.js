11722169bada51b3d9775ec317d9397193ccd7f5
bustlezach <bustlezach001@gmail.com>
2023-07-25 19:46:47 -0700

    task

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi.co/api/films/`+id;
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
