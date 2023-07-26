55e56646a287655b0446798749e857a3a9d98c35
bustlezach <bustlezach001@gmail.com>
2023-07-26 07:03:09 -0700

    task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,18 +3,22 @@ const request = require('request');
 const id = parseInt(process.argv[2]);
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-const message = function (error, response, body){
-        if(error){
-                console.error(error);
-        } else {
-                const characters = JSON.parse(body).characters;
-                characters.forEach((character) => {
-                        request(character, function(error, reponse, body){
-                                if (!error){
-                                        console.log(JSON.parse(body).name);
-                                }
-                        });
-                        });
-                }
+const printCharacters = function (characters, index) {
+  if (index >= characters.length) return;
+
+  request(characters[index], function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    } else {
+      printCharacters(characters, index + 1);
+    }
+  });
 };
-request(url, message);
+
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
