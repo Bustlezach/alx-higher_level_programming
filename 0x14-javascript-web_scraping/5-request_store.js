28fad94dfed0305e9ae2a83e4a9026b8aa91e09d
bustlezach <bustlezach001@gmail.com>
2023-07-25 18:57:12 -0700

    task

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
-
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const file = process.argv[3];
 
-request(url, function(error, response, body) {
-    if (error) {
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(file, body, 'utf8', function (error, data) {
+      if (error) {
         console.error(error);
-    } else {
-        const webContent = JSON.parse(body);
-        fs.writeFile(file, JSON.stringify(webContent), 'utf8', function(error) {
-            if (error) {
-                console.error(error);
-            }
-        });
-    }
+      }
+    });
+  }
 });
+
